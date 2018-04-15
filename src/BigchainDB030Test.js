6ee926759e13aec04bc92c690440673601a1ea3c
Eric van Riet Paap <ericvrp@gmail.com>
2018-04-15 22:34:58 +0200

    Post and search transactions in their simplest form

--- src/BigchainDB030Test.js
+++ src/BigchainDB030Test.js
@@ -4,41 +4,53 @@ import Button from 'material-ui/Button';
 const config = require('./config.json')
 // console.log(config)
 
+const driver = require('bigchaindb-driver') // https://github.com/bigchaindb/js-bigchaindb-driver
+
+const blockchain030Protocol1 = 'blockchain030Protocol1'
+
+
+// https://www.bigchaindb.com/developers/getstarted/
+const alice = new driver.Ed25519Keypair()
+const conn = new driver.Connection(config.apiRootEndpoint, { 
+  app_id: config.app_id,
+  app_key: config.app_key
+})
+
+// https://testnet.bigchaindb.com/docs
+const tx = driver.Transaction.makeCreateTransaction(
+  { 'def:type': blockchain030Protocol1,
+    message: 'This is a blockchain030 message' }, // asset
+  null, // meta data
+  [ driver.Transaction.makeOutput(driver.Transaction.makeEd25519Condition(alice.publicKey))], // output, spend it to ourself. // https://www.youtube.com/watch?v=zYa-GGNwxXo
+  alice.publicKey) // input publicKey
+
+
 class BigchainDB030Test extends Component {
-    handleLetsGo = () => {
-        // const message = "Yes sir!";
-        // console.log(message);
-        // alert(message);
+  handlePostTransaction = () => {
+    const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
+    conn.postTransactionSync(txSigned).then(tx => {
+      console.log(tx.id)
+      // alert(tx.id)
+    })
+  } // end of handlePostTransaction
 
-        // https://github.com/bigchaindb/js-bigchaindb-driver
-        const driver = require('bigchaindb-driver')
-        // console.log(driver)
+  handleSearchTransactions = () => {
+    conn.searchAssets(blockchain030Protocol1).then(assets => {
+      const s = JSON.stringify(assets, null, 2)
+      console.log(s)
+      // alert(s)
+    })
+  } // end of handleSearchTransactions
 
-        // https://www.bigchaindb.com/developers/getstarted/
-        const alice = new driver.Ed25519Keypair()
-        const conn = new driver.Connection(config.apiRootEndpoint, { 
-            app_id: config.app_id,
-            app_key: config.app_key
-        })
-
-        // https://testnet.bigchaindb.com/docs
-        const tx = driver.Transaction.makeCreateTransaction(
-            { message: 'This is a blockchain030 message' },
-            null,
-            [ driver.Transaction.makeOutput(
-                driver.Transaction.makeEd25519Condition(alice.publicKey))],
-            alice.publicKey)
-        const txSigned = driver.Transaction.signTransaction(tx, alice.privateKey)
-        // console.log(txSigned)
-        const x = conn.postTransactionSync(txSigned)
-        console.log(x)
-    } // end of handleLetsGo
-
-    render() {
-        return (
-            <Button variant="raised" onClick={this.handleLetsGo}>Let's go!</Button>
-        )
-    }
-}
+  render() {
+    return (
+      <span>
+        <Button variant="raised" onClick={this.handlePostTransaction}>Post transaction</Button>
+        <br /><br />
+        <Button variant="raised" onClick={this.handleSearchTransactions}>Search transactions</Button>
+      </span>
+    )
+  } // end of render
+} // end of class BigchainDB030Test
 
 export default BigchainDB030Test;
